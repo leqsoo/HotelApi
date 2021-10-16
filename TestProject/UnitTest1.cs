7f43ed16de53a335a6d741c73548e4e5f33015a4
leqso <aleqsandre.pataridze@gmail.com>
2021-10-16 21:42:10 +0400

    Folder Name Changed

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+using HotelApi.Controllers;
+using NUnit.Framework;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class CountryControllerTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void DeleteCountry_WhenCalled_DeleteCountryFromDatabase()
+        {
+            //var controller = new CountryController();
+        }
+    }
+}
---
+++ UnitTests/CountryControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelApi.Controllers;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class CountryControllerTests
+    {
+        [Test]
+        public void DeleteCountry_WhenCalled_DeleteCountryFromDatabase()
+        {
+            var controller = new CountryController();
+        }
+    }
+}
